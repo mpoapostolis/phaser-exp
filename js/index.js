e9eef3bff5ade8685598069c0411d1823c10b4b2
mpoapostolis <mpoapostolis@gmail.com>
2018-02-26 01:07:12 +0200

    phaser presentation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
-const game = new Phaser.Game(WIDTH, HEIGHT, Phaser.CANVAS, "gameArea");
+const game = new Phaser.Game(WIDTH, HEIGHT, Phaser.CANVAS);
 let stage;
 let hero;
 let cursors;
 let music;
+let pressClick;
 
-const idle = [...Array(10).keys()].map(num => `Idle${num}`);
+const idle = [...Array(10).keys()].map(num => `Idle${num}`); // [Idle0, Idle1, Idle2]
 const walk = [...Array(10).keys()].map(num => `Walk${num}`);
 const jump = [...Array(10).keys()].map(num => `Jump${num}`);
 const jumpAttack = [...Array(10).keys()].map(num => `JumpAttack${num}`);
@@ -14,39 +15,47 @@ const attack = [...Array(10).keys()].map(num => `Attack${num}`);
 const dead = [...Array(10).keys()].map(num => `Dead${num}`);
 
 const mainState = {
-  preload: function() {
+  preload() {
     game.load.image("stage", "images/bg.jpg");
     game.load.atlasJSONHash("hero", "images/hero.png", "images/hero.json");
   },
 
-  create: function() {
+  create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    // game.scale.setMaximum();
-    // game.scale.setScreenSize(true);
 
     stage = game.add.tileSprite(0, 0, WIDTH, HEIGHT, "stage");
     stage.scale.y = 1;
     stage.scale.x = 1;
+
     hero = game.add.sprite(200, 0, "hero", "Walk1");
     hero.width = 90;
     hero.height = 120;
-    hero.animations.add("walk", walk, 20, true, false);
-    hero.animations.add("idle", idle, 20, true, false);
-    hero.animations.add("jump", jump, 20, true, false);
-    hero.animations.add("attack", attack, 20, true, false);
-    hero.animations.add("dead", dead, 20, true, false);
+    hero.animations.add("walk", walk, 20, true);
+    hero.animations.add("idle", idle, 20, true);
+    hero.animations.add("jump", jump, 20, true);
+    hero.animations.add("attack", attack, 20, true);
+    hero.animations.add("dead", dead, 20, true);
     game.physics.arcade.enable(hero);
     hero.body.bounce.y = 0.1;
     hero.body.gravity.y = 300;
     hero.body.collideWorldBounds = true;
-    hero.anchor.set(0.5);
     cursors = game.input.keyboard.createCursorKeys();
-    stage.events.onInputDown.add(listener, this);
+    stage.inputEnabled = true;
+    stage.events.onInputDown.add(this.listener);
   },
 
-  update: function() {
+  listener(e) {
+    pressClick = true;
+  },
+
+  update() {
     hero.body.velocity.x = 0;
-    if (cursors.left.isDown) {
+
+    console.log(hero.animations._anims.attack.loopCount);
+    if (pressClick) {
+      hero.animations.play("attack");
+      if (hero.animations._anims.attack.loopCount > 0) pressClick = false;
+    } else if (cursors.left.isDown) {
       hero.body.velocity.x = -30;
       hero.animations.play("walk");
       stage.tilePosition.x += 2;
@@ -62,13 +71,8 @@ const mainState = {
       hero.animations.play("jump");
       hero.body.velocity.y = -250;
     }
-  },
-  render: function() {}
+  }
 };
-
-function listener() {
-  console.log("wtf");
-}
 
 game.state.add("mainState", mainState);
 game.state.start("mainState");
